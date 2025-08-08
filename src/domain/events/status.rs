5ba13f6f117b0e08a5fb7d91779e4e9c3a4c65d8
Joseph Wasden <jpwasden93@gmail.com>
2025-08-08 20:37:02 +0100

    Whitespace

--- src/domain/events/status.rs
+++ src/domain/events/status.rs
@@ -18,7 +18,6 @@ pub trait StatusEvent {
     fn bot_id(&self) -> String;
     fn state(&self) -> BotStates;
     async fn is_bot(&self) -> bool;
-
     async fn send_offline_warning(&self, entries: Vec<RegisterEntry>);
     async fn send_online_message(&self, entries: Vec<RegisterEntry>);
 }
