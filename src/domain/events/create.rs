0648b524d22b1858ce0455d6ce055f86011ad955
Joseph Wasden <jpwasden@gmail.com>
2025-08-09 13:03:41 +0100

    Added create tests

--- src/domain/events/create.rs
+++ src/domain/events/create.rs
@@ -5,6 +5,8 @@ use async_trait::async_trait;
 #[cfg(test)]
 use mockall::automock;
 
+
+#[cfg_attr(test, derive(Clone, Debug, PartialEq))]
 pub struct CreateEntry {
     pub user_id: String,
     pub bot_id: String,
@@ -44,11 +46,60 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::domain::register::MockRegister;
+    use crate::domain::register::{MockRegister, RegisterError};
+    use mockall::predicate::*;
 
     #[tokio::test]
     async fn test_add_to_register() {
-        let local = MockRegister::new();
-        let _app = App::new(local);
+        let user_id = String::from("user_id_12345");
+        let bot_id = String::from("bot_id_12345");
+
+        let entry = CreateEntry { user_id, bot_id, version: 1 };
+
+        let mut register = MockRegister::new();
+        register.expect_add().times(1).with(eq(entry.clone())).return_const(Ok(()));
+
+        let mut event = MockCreateEntryEvent::new();
+        event.expect_is_bot().times(1).return_const(true);
+        event.expect_entry().times(1).return_const(entry.clone());
+        event.expect_entry_added_message().times(1).return_const(());
+
+        let app = App::new(register);
+
+        app.add_to_register(event).await;
+    }
+
+    #[tokio::test]
+    async fn test_add_to_register_error() {
+        let user_id = String::from("user_id_12345");
+        let bot_id = String::from("bot_id_12345");
+
+        let entry = CreateEntry { user_id, bot_id, version: 1 };
+
+        let mut register = MockRegister::new();
+        register.expect_add().times(1).with(eq(entry.clone())).return_const(Err(RegisterError::EntryCreationError));
+
+        let mut event = MockCreateEntryEvent::new();
+        event.expect_is_bot().times(1).return_const(true);
+        event.expect_entry().times(1).return_const(entry.clone());
+        event.expect_failed_message().times(1).return_const(());
+
+        let app = App::new(register);
+
+        app.add_to_register(event).await;
+    }
+
+    #[tokio::test]
+    async fn test_add_to_register_not_a_bot() {
+        let mut register = MockRegister::new();
+        register.expect_add().times(0).return_const(Ok(()));
+
+        let mut event = MockCreateEntryEvent::new();
+        event.expect_is_bot().times(1).return_const(false);
+        event.expect_not_a_bot_message().times(1).return_const(());
+
+        let app = App::new(register);
+
+        app.add_to_register(event).await;
     }
 }
